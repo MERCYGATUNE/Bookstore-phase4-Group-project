a85369046b99ec16db3d6ec6df02a338fdba52c0
MERCYGATUNE <ariri691@gmail.com>
2024-07-10 14:06:06 +0300

    completed

--- server/main.py
+++ server/main.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors= CORS(app,origins="*")
 
 @app.route("/api/books", methods=['GET'])
 def books():
     return jsonify (
         {
             "books":[
-                'red riding',
-                'snow white'
+                'red ridinghood',
+                'snow white',
+                'cinderella'
                 
             ]
         }
     )
-if__name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=8000)    
